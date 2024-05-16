c488f6bde00e142d617679ad5723ea3513f8bfa7
KwonSanta <KwonSanta@gmail.com>
2024-05-16 10:03:57 +0900

    /api/someurl 의 get, post 요청 핸들러 메소드 작성 [연습 완료]

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -18,4 +18,16 @@ public class Controller40 {
     public void method2() {
         System.out.println("Controller40.method2");
     }
+
+    // todo : get, post  /api/someurl2
+    //        요청 처리 메소드 작성
+    @RequestMapping("/api/someurl2")
+    public void method3() {
+        System.out.println("Controller40.method3");
+    }
+
+    @PostMapping("/api/someurl2")
+    public void method4() {
+        System.out.println("Controller40.method4");
+    }
 }
